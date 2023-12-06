eef1eb0b9a64850da10d348fb6a83fec525a8926
Vasyl Boliuk <vbboliuk@gmail.com>
2023-12-06 08:50:48 +0200

    Add tests: plidrome number

--- src/tasks/palindrome_number.py
+++ src/tasks/palindrome_number.py
@@ -19,3 +19,18 @@ class PalindromeNumberSolution:
         if num_str == reverse_str:
             return True
         return False
+
+
+class TestPalindromeNumberSolution:
+
+    def test_simple_number(self):
+        assert PalindromeNumberSolution.is_palindrome(121) is True
+
+    def test_negative_number(self):
+        assert PalindromeNumberSolution.is_palindrome(-121) is False
+
+    def test_zero_number(self):
+        assert PalindromeNumberSolution.is_palindrome(0) is True
+
+    def test_number_negative_case(self):
+        assert PalindromeNumberSolution.is_palindrome(123) is False
